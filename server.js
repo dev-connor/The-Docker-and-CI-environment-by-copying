ce0be051d811d0dac3eeebc9a350f32a0d3d886b
Connor <kor.connor@gmail.com>
2023-04-14 15:00:00 +0900

    프리티어 작성 및 1부터 증가하는 섭 작성

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 // Express 모듈 불러오기
-const express = require('express')
-const redis = require('redis')
+const express = require("express")
+const redis = require("redis")
+
+// 레디스 클라이언트 생성
+const client = redis.createClient({
+  host: "redis-server",
+  port: 6379,
+})
 
 // Express App 생성하기
 const app = express()
 
-// 레디스 클라이언트 생성
-const client = redis.createClient({
-  host: 'redis-server',
-  port: 6379,
-})
+// 숫자는 0부터 시작합니다.
+client.set("number", 0)
 
-app.get('/', (req, res) => {
-  res.send('안녕하세요')
+app.get("/", (req, res) => {
+  client.get("number", (err, number) => {
+    res.status(200).send("숫자가 1씩 올라갑니다. 숫자: ", +number)
+    client.set("number", parseInt(number) + 1)
+  })
 })
 
 // Express App 시작하기
 app.listen(8080, () => {
-  console.log('애플리케이션이 실행됐습니다.')
+  console.log("애플리케이션이 실행됐습니다.")
 })
